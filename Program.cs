a2010b7879eaf6e3f50ccb113b794345aa80090d
qupchik <vojna.sasha@gmail.com>
2024-02-25 18:29:53 +0300

    решенил задачу и создал файл readme

--- Program.cs
+++ Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿Console.Write("Введите размер массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string[] stringArray = new string[m];
+
+void array(string[] stringArray)
+{
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-ю строку массива");
+        stringArray[i] = Console.ReadLine();
+    }
+}
+string[] threeSymbols(string[] stringArray)
+{
+    int n = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+            n++;
+    }
+    string[] resultArray = new string[n];
+    int j = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            resultArray[j] = stringArray[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+void printArray(string[] stringArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.Write($"\"{stringArray[i]}\", ");
+    }
+    Console.Write("]");
+}
+
+array(stringArray);
+printArray(threeSymbols(stringArray));
+
+
